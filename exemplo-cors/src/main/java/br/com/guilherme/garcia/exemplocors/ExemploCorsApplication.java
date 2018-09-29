0409c71025bfd983da4e8935a93800e70f4e660a
Guilherme Garcia <guilherme.alves@kestraa.com>
2018-09-29 20:56:24 -0300

    first commit

---
+++ exemplo-cors/src/main/java/br/com/guilherme/garcia/exemplocors/ExemploCorsApplication.java
@@ -0,0 +1,12 @@
+package br.com.guilherme.garcia.exemplocors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploCorsApplication.class, args);
+	}
+}
---
+++ exemplo-cors/src/test/java/br/com/guilherme/garcia/exemplocors/ExemploCorsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.guilherme.garcia.exemplocors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExemploCorsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
